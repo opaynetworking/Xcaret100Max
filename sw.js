d8e6440f56a945e0095014986afce8c7c0f19f68
Opay <56850117+opaynetworking@users.noreply.github.com>
2026-07-26 19:53:54 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,32 @@
+const CACHE_NAME = 'xcaret100-v1';
+const urlsToCache = [
+  '/index.html',
+  '/home.html',
+  'https://iili.io/CeJl8np.png'
+];
+
+// Install Service Worker and cache assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch cached assets when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
 
